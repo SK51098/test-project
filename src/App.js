205fcc29195a46f4366375c669ac6ec9dd873a32
satheesh kumar <satheeshkumarsk51098@gmail.com>
2023-08-05 16:14:56 +0530

    main file import

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Home from './pages/home/component';
+
+function App() {
+	return (
+		<Home />
+	);
+}
+
+export default App;
---
+++ src/pages/home/component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <header className="home-header">
+        <div className="d-none">hi sk</div>
+      </header>
+    </div>
+  );
+};
+
+export default Home;
